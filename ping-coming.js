ff748793d27f768f940fe008695a617a2b86e2e2
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-08-18 23:57:46 -0500

    ping-coming.js: Create errorMessage function to handle invalid/empty email address input; Add event listener to button

--- ping-coming.js
+++ ping-coming.js
@@ -1,1 +1,23 @@
-(() => {})();
+(() => {
+  "use strict";
+
+  // VARIABLES
+  const input = document.querySelector("input");
+  const emailAddress = document.querySelector("input").value;
+  const validEmailFormat =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const button = document.querySelector("button");
+  const root = document.querySelector(":root");
+  const colorStyles = window.getComputedStyle(root);
+  const errorColor = colorStyles.getPropertyValue("--error-color");
+
+  // FUNCTIONS
+  const errorMessage = (event) => {
+    if (!emailAddress.match(validEmailFormat) || emailAddress === "") {
+      event.preventDefault();
+      input.style.color = errorColor;
+    }
+  };
+  // EVENT LISTENER
+  button.addEventListener("click", errorMessage);
+})();
