c43b9e69ce653c6bbbc2da9b2c27084bba9519f1
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-08-20 21:34:01 -0500

    Styling error status for invalid email

--- ping-coming.js
+++ ping-coming.js
@@ -3,21 +3,28 @@
 
   // VARIABLES
   const input = document.querySelector("input");
-  const emailAddress = document.querySelector("input").value;
+  const emailAddress = input.value;
   const validEmailFormat =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   const button = document.querySelector("button");
   const root = document.querySelector(":root");
   const colorStyles = window.getComputedStyle(root);
   const errorColor = colorStyles.getPropertyValue("--error-color");
+  //   const fontColor = colorStyles.getPropertyValue("--font-color");
 
   // FUNCTIONS
   const errorMessage = (event) => {
     if (!emailAddress.match(validEmailFormat) || emailAddress === "") {
       event.preventDefault();
-      input.style.color = errorColor;
+      input.style.borderColor = errorColor;
     }
   };
-  // EVENT LISTENER
+
+  //   const hover = (event) => {
+  //     document.querySelector("button");
+  //   };
+
+  // EVENT LISTENERS
   button.addEventListener("click", errorMessage);
+  button.addEventListener("mouseover", hover);
 })();
